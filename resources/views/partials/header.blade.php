398b3266884227874a708e33c86c9d614c9d5355
Alex <alexmikhajlovic@gmail.com>
2021-03-16 17:05:03 +0100

    Init

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            
+          <!-- <a class="navbar-brand" href="#">Logo</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button> -->
+
+          <div class="collapse navbar-collapse" id="navbarNav">
+
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" class="{{ (Request::route()->getName() == 'home') ? 'active'  : '' }}" aria-current="page" href="{{ route('home') }}"><i class="fas fa-home"></i> Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" class="{{ (Request::route()->getName() == 'products') ? 'active'  : '' }}" href="{{ route('products') }}">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" class="{{ (Request::route()->getName() == 'about') ? 'active'  : '' }}" href="{{ route('about') }}">About</a>
+              </li>
+              <!-- <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><i class="fas fa-user"></i></a>
+              </li> -->
+            </ul>
+
+          </div>
+
+        </div>
+      </nav>
+    
+</header>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.app')
+
+@section('title', 'Products')
+
+@section('content')
+
+<h1>Products</h1>
+
+@endsection
